a2af1b756bb32485289396f2e39b44d6b59eb9a3
RevenkoVladislav <Vlad_reven@mail.ru>
2026-01-08 18:46:20 +0600

    Стартовые компоненты, роутинг, модель и миграция: Э

---
+++ resources/js/Pages/Post/Index.vue
@@ -0,0 +1,24 @@
+<script>
+import { Link } from '@inertiajs/vue3';
+export default {
+    name: "Index",
+    components: {
+        Link
+    }
+}
+</script>
+
+<template>
+    <div class="w-96 mx-auto pt-8">
+        <h1 class="text-lg">
+            Posts
+        </h1>
+        <div>
+            <Link class="bg-sky-500 rounded-full text-center text-white">Add Post</Link>
+        </div>
+    </div>
+</template>
+
+<style scoped>
+
+</style>
